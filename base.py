4376b5bfe7ffdd442c3f441205e092cd9beade58
gustavo-yugen <gustavoyugenito@gmail.com>
2025-09-19 16:31:01 -0300

    base-dados-web
    base de dados do web scraping, da marca the saint

--- base.py
+++ base.py
@@ -19,12 +19,3 @@ def get_html (url: str):
 def get_aba (html: BeautifulSoup):
     html_das_abas = html.find('div', class_="sections nav-sections")
     return html_das_abas
-
-link = "https://thesaint.com.br/?fbclid=PAdGRleAM6dthleHRuA2FlbQIxMQABp18Ga0zr6ehn3HhTWQo4q4U6HFVsJLSXWDHSundAr9d1SIibHdPVv6mU1rmw_aem_AcsO6loHqMKKB9qBOcEmpg"
-
-htmll = get_html(link)
-
-dados_abas = get_aba(htmll)
-
-
-print(dados_abas)
